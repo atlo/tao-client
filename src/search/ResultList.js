f8f7dd6468695ca8e80a0f5bad8d95ab3e6b8d90
László Horváth <bedafoltyn@gmail.com>
2019-01-14 17:16:48 +0100

    results list only shows file name

--- src/search/ResultList.js
+++ src/search/ResultList.js
@@ -1,10 +1,16 @@
 import React from 'react'
 import HighlightList from './HighlightList'
 
+function formatfileName (path) {
+  const splitted = path.split('/')
+
+  return splitted[splitted.length - 1]
+}
+
 export default function ({ files }) {
   const results = files.map((result, index) => (
     <li key={index}>
-      <a href='#' target='_blank'>{result.fileName}</a>
+      <a href='#' target='_blank'>{formatfileName(result.fileName)}</a>
       <HighlightList highlights={result.highlights} />
     </li>
   ))
